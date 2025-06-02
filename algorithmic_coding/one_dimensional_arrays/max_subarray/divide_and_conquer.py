d3d6d3e1a16fe63f8684dbc84f2e6285e5035f36
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-01 23:38:09 -0400

    update code

--- algorithmic_coding/one_dimensional_arrays/max_subarray/divide_and_conquer.py
+++ algorithmic_coding/one_dimensional_arrays/max_subarray/divide_and_conquer.py
@@ -1,9 +1,15 @@
 # max subaray by using divide and conquer O(n*log(n)) time and O(n) space
+from socket import fromfd
 
 # divide the given array into two halves and return the maximum of following three:
 # 1, Maximum subarray sum in left half.
 # 2. Maximum subarray sum in the right half
 # 3. Maximum subarray sum such that the subarray crosses the midpoint
+# Maximum subarray in left and right halves can be found easily by two recursive calls.
+# To find maximum subarray sum such that the subarray crosses the midpoint, find the maximum sum
+# starting from mid point and ending at some point on the left of mid, then find the maximum sum
+# starting from mid + 1 and ending with some point on right of mid + 1. Finally, combine the two and
+# return the maximum among left, right and combination of both.
 
 def max(a, b, c=None):
     if c is None:
