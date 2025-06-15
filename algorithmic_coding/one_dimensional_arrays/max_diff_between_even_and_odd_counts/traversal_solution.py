e57a1397119aab06521c0e2156518e905fb2689d
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-15 16:13:17 -0400

    update solution

--- algorithmic_coding/one_dimensional_arrays/max_diff_between_even_and_odd_counts/traversal_solution.py
+++ algorithmic_coding/one_dimensional_arrays/max_diff_between_even_and_odd_counts/traversal_solution.py
@@ -23,25 +23,29 @@ class TraversalSolution:
         freq = self._charFrequencies(subs) 
         odds = set()
         evens = set()
+        even_min = 0
         for val in freq.values():
             if val % 2 == 0:
                 evens.add(val)
             else:
                 odds.add(val)
         if len(odds) > 0:
-           odd_max = max(odds)
+            odd_max = max(odds)
         else:
-           odd_max = 0
+            odd_max = 0
         if len(evens) > 0:
-           even_min = min(evens)
+            even_min = min(evens)
         else:
            # find if there is even count from position k+i+1 to the end of the string
-           for j in range(self.k+i+2,self.n+1):
-               subs2 = s[k+i:j]
-               if subs[0] == subs[1]:
-                   evens_min = min(evens)
-                   break
-
+            for j in range(self.k+i+2,self.n+1):
+                subs2 = s[k+i:j]
+                if subs[0] == subs[1]:
+                    evens_min = min(evens)
+                    break
+            
+            if even_min == 0:
+                raise ValueError(f"invalid input string {s}")
+        
         return odd_max - even_min
 
     def maxDifference(self, s: str, k: int) -> int:
@@ -53,7 +57,7 @@ class TraversalSolution:
 
         max_diffs = list()
         for i in range(0,self.n-self.k+1):
-            max_diffs.append(self._maxDifferenceSubs(subs, i))
+            max_diffs.append(self._maxDifferenceSubs(s, i))
         return max(max_diffs)
             
 
