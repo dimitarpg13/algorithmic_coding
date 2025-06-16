031575112113f045f15fd082e58225ad4f8ce8ee
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-16 18:40:34 -0400

    update solution

--- algorithmic_coding/one_dimensional_arrays/max_diff_between_even_and_odd_counts/traversal_solution.py
+++ algorithmic_coding/one_dimensional_arrays/max_diff_between_even_and_odd_counts/traversal_solution.py
@@ -13,6 +13,17 @@ class TraversalSolution:
         """
         return dict(Counter(s))
 
+    def _getMaxOddFrequency(freq1: dict, freq2: dict) -> int:
+        """
+        It merges the two frequency dicts and returns the new 
+        """
+        result = dict()
+        n1 = len(freq1)
+        n2 = len(freq2)
+        if n1 < n2:
+
+
+
     def _maxDifferenceSubs(self, s: str, i: int) -> int:
         """
         type s: str, input string
@@ -36,17 +47,40 @@ class TraversalSolution:
         if len(evens) > 0:
             even_min = min(evens)
         else:
-           # find if there is even count from position k+i+1 to the end of the string
+            # find if there is even count from position k+i+1 to the end of the string
+            max_diff = None
+            new_freq = dict()
             for j in range(self.k+i+2,self.n+1):
-                subs2 = s[k+i:j]
-                if subs[0] == subs[1]:
+                subs2 = s[self.k+i:j]
+
+                if subs[0] == subs[1] and subs[0] not in freq:
                     evens_min = min(evens)
+                    max_diff = odds_max - evens_min
                     break
-            
+                else:
+                    if not new_freq:
+                        new_freq[subs[0]] = 1
+                        new_freq[subs[1]] = 1
+                    else:
+                        if subs[-1] in new_freq:
+                            new_freq[subs[-1]] += 1
+                            evens_min = 2
+                    
+                            odds_max_new = self._getMaxOddFrequency(freq, new_freq)
+                            if odds_max_new == odd_max:
+                                max_diff = odds_max - evens_min
+                                break
+                            else:
+                                if max_diff is None:
+                                    max_diff = odds_max_new - evens_min
+                                else:
+                                    max_diff = max(max_diff, odds_max_new - evens_min)
+                        else:
+                            new_freq[subs[-1]] = 1
+                
             if even_min == 0:
-                raise ValueError(f"invalid input string {s}")
-        
-        return odd_max - even_min
+                raise ValueError(f"invalid input string! The even frequency count is zero: {s}")
+        return max_diff
 
     def maxDifference(self, s: str, k: int) -> int:
         """
@@ -55,10 +89,10 @@ class TraversalSolution:
         self.k = k
         self.n = len(s)
 
-        max_diffs = list()
+        max_diff = float('-inf')
         for i in range(0,self.n-self.k+1):
-            max_diffs.append(self._maxDifferenceSubs(s, i))
-        return max(max_diffs)
+            max_diff = max(max_diff,self._maxDifferenceSubs(s, i))
+        return max_diff
             
 
 if __name__ == '__main__':
