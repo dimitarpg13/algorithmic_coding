aa11f5afceb1dae43460308013f2c4fa950fd5e6
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-19 20:39:33 -0400

    Update traversal_solution.py

--- algorithmic_coding/one_dimensional_arrays/max_diff_min_count_partitions/traversal_solution.py
+++ algorithmic_coding/one_dimensional_arrays/max_diff_min_count_partitions/traversal_solution.py
@@ -1,14 +1,20 @@
-class TraversalSolution:
+class Solution:
     def __init__(self):
         self.nums = None
         self.k = None 
         self.seqs = None
+        # seqs are stored in a list of tuples in which the first
+        # element encodes the seq min, tehe second element - the seq
+        # max, and the third tuple element stores the sequence as a
+        # list  
 
     # Conjecture: let s1 and s2 be any two subsequences resulting from
     # the partition of the original array `nums`. Then every element of 
     # one of the sequence is larger than every element of the other.
+    
 
-    def partition_array(self, nums: List[int], k: int) -> int:
+
+    def partitionArray(self, nums: List[int], k: int) -> int:
         """
         nums: input array to be partitioned
         k: max difference between any pair of elements in  
@@ -16,6 +22,10 @@ class TraversalSolution:
         """
         self.nums = nums
         self.k = k
-        self.seqs = dict()   
+        self.seqs = dict()
+        cur_seq = list()   
         for i, val in enumerate(nums):
-            pass
+            if i == 0:
+                cur_seq.append(val)
+            else:
+                pass
