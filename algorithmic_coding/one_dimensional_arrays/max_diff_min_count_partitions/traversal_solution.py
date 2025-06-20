b8ca51c7d9fb3e528ebb48007c79e39944ab0b39
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-20 13:13:53 -0400

    update solution

--- algorithmic_coding/one_dimensional_arrays/max_diff_min_count_partitions/traversal_solution.py
+++ algorithmic_coding/one_dimensional_arrays/max_diff_min_count_partitions/traversal_solution.py
@@ -1,18 +1,27 @@
+from typing import List
+
 class Solution:
     def __init__(self):
         self.nums = None
         self.k = None 
         self.seqs = None
         # seqs are stored in a list of tuples in which the first
-        # element encodes the seq min, tehe second element - the seq
+        # element encodes the seq min, the second element - the seq
         # max, and the third tuple element stores the sequence as a
         # list  
 
-    # Conjecture: let s1 and s2 be any two subsequences resulting from
-    # the partition of the original array `nums`. Then every element of 
-    # one of the sequence is larger than every element of the other.
-    
+    # Definition: optimal partition
+    # A partition of the original sequence s into subsequences s1, s2, ..., sn
+    # such that for each subsequence the max range within it is at most k and no partition
+    # exist resulting in a smaller number of sequences having this range constraint.
 
+    # Conjecture: We can always find an optimal partition in which for any two subsequences
+    # s1 and s2  every element of one of the sequence is larger than every element of the
+    # other.
+    
+    def find_prev_seq(val: int, seqs: List[tuple]) -> tuple:
+        for cur in reversed(seqs):
+            if cur[0] <= val <= cur[1]:
 
     def partitionArray(self, nums: List[int], k: int) -> int:
         """
@@ -28,6 +37,7 @@ class Solution:
         cur_max = float('-inf')
         cur_min_idx = -1
         cur_max_idx = -1 
+        prev_seq = None
         for i, val in enumerate(nums):
             if i == 0:
                 cur_seq.append(val)
@@ -45,5 +55,8 @@ class Solution:
                         cur_max_idx = i
                      cur_seq.append(val)
                  elif val < cur_min:
-
+                     
                  elif val > cur_max:
+
+
+
