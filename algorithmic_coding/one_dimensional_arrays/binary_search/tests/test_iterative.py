01f3b5f344d1e32561dd89a434a5537d5188534a
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-28 17:39:46 -0400

    add unit test

---
+++ algorithmic_coding/one_dimensional_arrays/binary_search/tests/test_iterative.py
@@ -0,0 +1,20 @@
+import unittest
+from one_dimensional_arrays.binary_search.iterative import _binary_search
+
+
+class IterativeSolutionTestCase(unittest.TestCase):
+    def test_iterative(self):
+        arr = [-2, -1, 0, 1, 7, 11, 115, 223]
+        x = -1
+        expected = 1
+        result = _binary_search(arr, 0, len(arr) - 1, x)
+        if result != -1:
+            print(f"Element is present at index {result}")
+            self.assertEqual(result, expected, f"Expected index {expected}")
+        else:
+            print("Element is not present in array")
+            self.assertEqual(result, -1, "Element should not be present in array")
+
+
+if __name__ == '__main__':
+    unittest.main()
