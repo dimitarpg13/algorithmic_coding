045b638c188b9a2f5dfb1850b8c42ec6dd06deb4
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-28 17:12:56 -0400

    update code

--- algorithmic_coding/one_dimensional_arrays/binary_search/iterative.py
+++ algorithmic_coding/one_dimensional_arrays/binary_search/iterative.py
@@ -1,6 +1,15 @@
 from utils import get_mid
+from enum import IntFlag
+from typing import List, Optional
 
-def binary_search(arr, left, right, x):
+class SearchType(IntFlag):
+    RETURN_INVALID_IF_MISSING = 1
+    RETURN_CLOSEST_ON_LEFT_IF_MISSING = 2
+    RETURN_CLOSEST_ON_RIGHT_IF_MISSING = 4
+    ARR_DEFINED_WITH_INDICES_OF_FIRST_AND_LAST = 8
+    ARR_DEFINED_WITH_INDEX_OF_FIRST_AND_LENGTH = 16
+
+def _binary_search(arr: List, left: int, right: int, x: float) -> int:
     """
     Perform binary search on a sorted array.
     :param arr: List of sorted elements
@@ -25,26 +34,26 @@ def binary_search(arr, left, right, x):
     return -1
 
 
-def binary_search2(arr, left, right, x):
+def _binary_search2(arr: List, left: int, length: int, x: float) -> int:
     """
     Perform binary search on a sorted array.
     :param arr: List of sorted elements
     :param left: Left index of the subarray to search
-    :param right: Right index of the subarray to search
+    :param length: Length of the subarray to search
     :param x: Element to search for
     :return: Index of x in arr if found, otherwise -1
     """
-    while left <= right + 1:
-       mid = get_mid(left, right + 1)
+    while left <= length:
+       mid = get_mid(left, length)
 
-       if mid == left or mid == right + 1:
+       if mid == left or mid == length:
            return -1
 
        if arr[mid] == x:
            return mid
        
        elif arr[mid] > x:
-           right = mid - 1
+           length = mid
 
        else:
            left = mid
@@ -58,7 +67,7 @@ if __name__ == '__main__':
     print("testing binary_search")
     x = -1
     expected = 1
-    result = binary_search(arr, 0, len(arr)-1, x)
+    result = _binary_search(arr, 0, len(arr)-1, x)
     if result != -1:
         print(f"Element is present at index {result}")
         assert result == expected, f"Expected index {expected}"
@@ -69,7 +78,7 @@ if __name__ == '__main__':
  
     x = 29
     expected = 6
-    result = binary_search(arr2, 0, len(arr2)-1, x)
+    result = _binary_search(arr2, 0, len(arr2)-1, x)
     if result != -1:
         print(f"Element is present at index {result}")
         assert result == expected, f"Expected index {expected}"
@@ -78,7 +87,7 @@ if __name__ == '__main__':
     
     x = 23
     expected = -1
-    result = binary_search(arr2, 0, len(arr2)-1, x)
+    result = _binary_search(arr2, 0, len(arr2)-1, x)
     if result == expected:
         print(f"Element {x} is not present at the array")
     else:
@@ -86,7 +95,7 @@ if __name__ == '__main__':
 
     x = 61
     expected = -1
-    result = binary_search(arr2, 0, len(arr2)-1, x)
+    result = _binary_search(arr2, 0, len(arr2)-1, x)
     if result == expected:
         print(f"Element {x} is not present at the array")
     else:
@@ -95,7 +104,7 @@ if __name__ == '__main__':
     print("testing binary_search2")
     x = -1
     expected = 1
-    result = binary_search(arr, 0, len(arr)-1, x)
+    result = _binary_search(arr, 0, len(arr)-1, x)
     if result != -1:
         print(f"Element is present at index {result}")
         assert result == expected, f"Expected index {expected}"
@@ -104,7 +113,7 @@ if __name__ == '__main__':
 
     x = 29
     expected = 6
-    result = binary_search2(arr2, 0, len(arr2)-1, x)
+    result = _binary_search2(arr2, 0, len(arr2), x)
     if result != -1:
         print(f"Element is present at index {result}")
         assert result == expected, f"Expected index {expected}"
@@ -112,14 +121,14 @@ if __name__ == '__main__':
         print("Element is not present in array")
     x = 23
     expected = -1
-    result = binary_search2(arr2, 0, len(arr2)-1, x)
+    result = _binary_search2(arr2, 0, len(arr2), x)
     if result == expected:
         print(f"Element {x} is not present at the array")
     else:
         assert False, f"Element {x} is found but it should not be present in array"
     x = 61
     expected = -1
-    result = binary_search2(arr2, 0, len(arr2)-1, x)
+    result = _binary_search2(arr2, 0, len(arr2), x)
     if result == expected:
         print(f"Element {x} is not present at the array")
     else:
