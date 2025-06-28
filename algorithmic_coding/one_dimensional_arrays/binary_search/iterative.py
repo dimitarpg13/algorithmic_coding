a2831c093b3fc734f3133f7bd17afb43b5600604
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-28 00:13:30 -0400

    Update iterative.py

--- algorithmic_coding/one_dimensional_arrays/binary_search/iterative.py
+++ algorithmic_coding/one_dimensional_arrays/binary_search/iterative.py
@@ -7,27 +7,81 @@ def binary_search(arr, left, right, x):
     :param x: Element to search for
     :return: Index of x in arr if found, otherwise -1
     """
-    
-    while left <= right:
-        mid = left + (right - left) // 2
         
-        if arr[mid] == x:
-            return mid 
-        
-        elif arr[mid] < x:
-            left = mid + 1
+	while start <= end:
+        mid = get_mid(start, end)
+				
+        if lst[mid] == trg:
+            return mid
+		
+        elif lst[mid] > trg:
+            right = mid - 1
         
         else:
-            right = mid - 1
-            
-    return -1
+            left = mid + 1
+
+		return -1
+
+
+def binary_search2(arr, left, right, x):
+    """
+    Perform binary search on a sorted array.
+    :param arr: List of sorted elements
+    :param left: Left index of the subarray to search
+    :param right: Right index of the subarray to search
+    :param x: Element to search for
+    :return: Index of x in arr if found, otherwise -1
+    """
+	while left <= right+1:
+
+       mid = get_mid(left, right + 1)
+
+       if mid == left or mid == right + 1:
+           return -1
+
+       if arr[mid] == x:
+           return mid
+       
+       elif arr[mid] > x:
+           right = mid - 1
+
+       else
+           left = mid
+         
+	return -1
+
 
 if __name__ == '__main__':
     arr = [-2, -1, 0, 1, 7, 11, 115, 223]
+
     x = -1
-    
+    expected = 1
     result = binary_search(arr, 0, len(arr)-1, x)
     if result != -1:
-        print("Element is present at index", result)
+        print(f"Element is present at index {result}")
+        assert(result == expected, f"Expected index {expected")
     else:
         print("Element is not present in array")
+
+    arr2 = [1, 5, 9, 11, 17, 21, 29, 33, 59]
+ 
+    x = 29
+    expected = 6
+    result = binary_search(arr2, len(arr2)-1, x)
+    if result != -1:
+        print(f"Element is present at index {result}")
+        assert result == expected, f"Expected index {expected}"
+    else:
+        assert False, "Element is not present in array"
+    
+    x = 23
+    expected = -1
+    result = binary_search(arr2, len(arr2), x)
+    if result != -1:
+        print(f"Element is present at index {result}")
+        assert result == expected, f"Expected index {expected}"
+    else:
+        assert False, "Element is not present in array"
+
+trg_61 = 61
+res_61 = find_index(trg_61, lst)
