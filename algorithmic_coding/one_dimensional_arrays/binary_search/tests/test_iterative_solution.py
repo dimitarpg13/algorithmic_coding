52a3772f218b856fa5dfb3b8f088edbd0b05e0a7
Dimitar Gueorguiev <dimitar_pg13@hotmail.com>
2025-06-28 20:45:30 -0400

    update code

--- algorithmic_coding/one_dimensional_arrays/binary_search/tests/test_iterative_solution.py
+++ algorithmic_coding/one_dimensional_arrays/binary_search/tests/test_iterative_solution.py
@@ -1,12 +1,14 @@
 import unittest
+from parameterized import parameterized
 from one_dimensional_arrays.binary_search.iterative_solution import _binary_search
 
 
+
 class IterativeSolutionTestCase(unittest.TestCase):
-    def test_iterative(self):
-        arr = [-2, -1, 0, 1, 7, 11, 115, 223]
-        x = -1
-        expected = 1
+    @parameterized.expand([
+            ([-2, -1, 0, 1, 7, 11, 115, 223], -1, 1),  # (array, x, expected)
+        ])
+    def test_iterative(self, arr, x, expected):
         result = _binary_search(arr, 0, len(arr) - 1, x)
         if result != -1:
             print(f"Element is present at index {result}")
